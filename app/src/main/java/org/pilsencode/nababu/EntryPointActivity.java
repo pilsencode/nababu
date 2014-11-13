8ebbd6018995709ca44e2fdccf0cc0c8b120c6ff
veny <vaclav.sykora@gmail.com>
2014-11-13 20:14:30 +0100

    connected 2 activities into flow

--- app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
+++ app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
@@ -2,23 +2,51 @@ package org.pilsencode.nababu;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by veny on 13.11.14.
  */
 public class EntryPointActivity extends Activity {
 
+    private EditText txtUserName;
+    private Button btnJoinGame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_entry_point);
+
+        txtUserName = (EditText) findViewById(R.id.edit_username);
+        btnJoinGame = (Button) findViewById(R.id.button_join_game);
+
+        btnJoinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EntryPointActivity.this, "Not implemented yet", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /** Called when the user clicks the 'Host Game' button */
+    public void hostGame(View view) {
+        if (0 == txtUserName.getText().toString().trim().length()) {
+            Toast.makeText(EntryPointActivity.this, "Username cannot be empty", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(this, PlayingFieldActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
