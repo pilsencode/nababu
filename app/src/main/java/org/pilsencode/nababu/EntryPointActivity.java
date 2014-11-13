a4b3cf3bd794de8e70fee8184e48f101fc959aa2
veny <vaclav.sykora@gmail.com>
2014-11-13 21:32:37 +0100

    refactoring: 2 layouts for entry point activity

--- app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
+++ app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
@@ -10,8 +10,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 /**
@@ -26,7 +28,7 @@ public class EntryPointActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_entry_point);
+        setContentView(R.layout.layout_entry_point);
 
         txtUserName = (EditText) findViewById(R.id.edit_username);
         btnJoinGame = (Button) findViewById(R.id.button_join_game);
@@ -34,7 +36,7 @@ public class EntryPointActivity extends Activity {
         btnJoinGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(EntryPointActivity.this, "Not implemented yet", Toast.LENGTH_LONG).show();
+                Toast.makeText(EntryPointActivity.this, "Not implemented yet", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -42,11 +44,24 @@ public class EntryPointActivity extends Activity {
     /** Called when the user clicks the 'Host Game' button */
     public void hostGame(View view) {
         if (0 == txtUserName.getText().toString().trim().length()) {
-            Toast.makeText(EntryPointActivity.this, "Username cannot be empty", Toast.LENGTH_LONG).show();
+            Toast.makeText(EntryPointActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
         } else {
-            Intent intent = new Intent(this, PlayingFieldActivity.class);
-            startActivity(intent);
+            setContentView(R.layout.layout_host_game);
+
+            // http://javatechig.com/android/android-listview-tutorial
+            String[] values = new String[] { "veny", "tomor", "ondra" };
+            // use your custom layout
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+            ListView listPlayers = (ListView) findViewById(R.id.list_of_players);
+            listPlayers.setAdapter(adapter);
         }
+    }
+
+    /** Called when the user clicks the 'Start' button */
+    public void startGame(View view) {
+        Intent intent = new Intent(this, PlayingFieldActivity.class);
+        startActivity(intent);
     }
 
 }
