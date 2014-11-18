db21a3ff0c9d0eb4992611070556b7fc740b901b
veny <vaclav.sykora@gmail.com>
2014-11-18 22:51:45 +0100

    first success by BT discovering

--- app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
+++ app/src/main/java/org/pilsencode/nababu/EntryPointActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -111,7 +113,6 @@ public class EntryPointActivity extends Activity {
         // PAIRED DEVICES
 
         pairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_in_list);
-
         ListView pairedListView = (ListView) findViewById(R.id.list_of_paired_devices);
         pairedListView.setAdapter(pairedDevicesArrayAdapter);
 
@@ -130,14 +131,24 @@ public class EntryPointActivity extends Activity {
         // NEW DEVICES
 
         newDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_in_list);
+        ListView newListView = (ListView) findViewById(R.id.list_of_new_devices);
+        newListView.setAdapter(newDevicesArrayAdapter);
 
-//        setProgressBarIndeterminateVisibility(true);
+        // Register for broadcasts when a device is discovered
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(mReceiver, filter);
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
+
+        setProgressBarIndeterminateVisibility(true);
 //        // If we're already discovering, stop it
 //        if (bluetoothAdapter.isDiscovering()) {
 //            bluetoothAdapter.cancelDiscovery();
 //        }
-//        // Request discover from BluetoothAdapter
-//        bluetoothAdapter.startDiscovery();
+        // Request discover from BluetoothAdapter
+        bluetoothAdapter.startDiscovery();
     }
 
     /** Called when the user clicks the 'Start' button */
@@ -188,6 +199,30 @@ public class EntryPointActivity extends Activity {
             default:
                 super.onActivityResult(requestCode, resultCode, data);
         }
-
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // If it's already paired, skip it, because it's been listed already
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    newDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            // When discovery is finished
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                setProgressBarIndeterminateVisibility(false);
+//                setTitle(R.string.select_device);
+                if (newDevicesArrayAdapter.getCount() == 0) {
+                    String noDevices = getResources().getText(R.string.none_paired).toString();
+                    newDevicesArrayAdapter.add(noDevices);
+                }
+            }
+        }
+    };
 }
