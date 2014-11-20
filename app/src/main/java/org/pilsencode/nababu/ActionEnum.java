f43042b77f522acc06911e0f8c491f6af76dd884
veny <vaclav.sykora@gmail.com>
2014-11-20 18:49:18 +0100

    added enum with remote actions

---
+++ app/src/main/java/org/pilsencode/nababu/ActionEnum.java
@@ -0,0 +1,13 @@
+package org.pilsencode.nababu;
+
+/**
+ * Enumeration representing list of actions communicated via remote connection.
+ *
+ * Created by veny on 20.11.14.
+ */
+public enum ActionEnum {
+
+    JOIN,
+    MOVE
+
+}
